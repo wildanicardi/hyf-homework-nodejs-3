9ec133fb0eee67585b33d3d2200c8f5c1d2c1a3b
wildanicardi <wildanicardi9@gmail.com>
2020-06-09 10:28:25 +0700

    add get users

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const user = {
+  name:"Ali WIldan",
+  email:"ali@mail.com"
+}
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/users',(req,res) => {
+  res.send(user);
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
