67a0c75a70349f7b1ff8d1700409be13be004a7b
wildanicardi <wildanicardi9@gmail.com>
2020-06-09 10:59:32 +0700

    edit response get users

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const user = {
-  name:"Ali WIldan",
-  email:"ali@mail.com"
-}
+const users = []
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/users',(req,res) => {
-  res.send(user);
+app.get('/users', (req, res) => {
+  res.json(users);
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
